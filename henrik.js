4063129c33539405a5ebede039cc19b700fb3633
Henrik Mader <henrikmader@Henriks-Air.fritz.box>
2020-12-04 18:47:54 +0100

    "hi"

---
+++ henrik.js
@@ -0,0 +1,50 @@
+const Jimp = require("jimp");
+//Average von r und g und b nehmen
+
+
+const LOWRESWIDTH = 10;
+const LOWRESHEIGHT = 10;
+
+async function readImageFromFile(filename) {
+    return await Jimp.read(filename);
+  }
+
+class Pixel {
+    static properties = ["r", "g", "b", "l", "s" /*"rg", "gb", "br" */];
+    constructor(r = 0, g = 0, b = 0, posx = 0, posy = 0) {
+      r = r / 256;
+      g = g / 256;
+      b = b / 256;
+      let maxrgb = Math.max(r, g, b);
+      let minrgb = Math.min(r, g, b);
+      this.r = r;
+      this.g = g;
+      this.b = b;
+      this.l = (maxrgb + minrgb) / 2;
+      this.s = (maxrgb - minrgb) / (1 - Math.abs(2 * this.l - 1));
+      //this.pureb =
+  
+      // this.rg = (r - g + 1) / 2;
+      // this.gb = (g - b + 1) / 2;
+      // this.br = (b - r + 1) / 2;
+    }
+  }
+
+  function ImageToLowResolutionPixelList(img) {
+    let pixelList = [];
+    let imgsm = img.resize(LOWRESWIDTH, LOWRESHEIGHT);
+    for (let i = 0; i < LOWRESWIDTH; i++) {
+      for (let j = 0; j < LOWRESHEIGHT; j++) {
+        let { r, g, b } = Jimp.intToRGBA(imgsm.getPixelColor(i, j));
+        let pix = new Pixel(r, g, b, i, j);
+        pixelList.push(pix);
+      }
+    }
+    return pixelList;
+  }
+
+  img = readImageFromFile("/Users/henrikmader/Desktop/Images/image-distance-experiments/images/img1.jpg")
+
+  let pixelList = ImageToLowResolutionPixelList(img);
+
+  console.log(pixelList)
