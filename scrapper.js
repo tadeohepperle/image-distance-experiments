a37696d0be05455549c5a778be7041edfd5594fe
Henrik Mader <henrikmader@Henriks-Air.fritz.box>
2020-12-08 22:20:42 +0100

    "hi"

--- scrapper.js
+++ scrapper.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
 let fs = require("fs");
+const filepath = "pics2.json";
 
-async function get (variable) {
+async function get(variable) {
 
     //1-30
     let res = await axios.get(`https://api.unsplash.com/search/photos?page=${variable}&query=bar`, {
@@ -9,26 +10,55 @@ async function get (variable) {
       Authorization: "Client-ID wfu6GTb5lA-wRnYKp4WSk72EroMBCts76Te_WgqwJMY"
     }
 });
-    console.log(res)
+    console.log(res.data)
     return res;
 }
 
 async function run () {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 10; i < 50; i++) {
     let json = await get(i);
-    console.log(json.data)
-    let str = JSON.stringify(json.data);
+    if (json.data && json.data.results && json.data.results.length > 0) {
 
-    fs.writeFile("pics.json", str, function(error) {
-        if (error) {
-            console.log("Error");
-        } else {
-            console.log("Success");
+
+        let results = json.data.results;
+
+        let content = await fs.promises.readFile(filepath, "utf-8");
+        //"[]"
+
+        //Erstellt Object
+        /*
+        "[]"
+        {
+            []
         }
+        */
+        let arr = JSON.parse(content)  
+        arr = [...arr, ...results]
+
+        await fs.promises.writeFile(filepath, JSON.stringify(arr), "utf-8");
+
+        
     }
-    );
-}
+    }
 }
 
-run()
+async function readfileAndConvertToURL (filepath){
+    let urlArray = [];
+    let stringFile = (await fs.promises.readFile(filepath, "utf-8"))
+    //War davor kein JSON File
+    let file = JSON.parse(stringFile)
+    for (let i = 0; i < file.length - 1; i++) {
+        let id = "";
+        let url = "";
+
+        url = file [i].urls.raw
+        id = file [i].id;
+
+        urlArray = [...urlArray, {url, id}]
+    }
+    console.log(urlArray)
+    return urlArray;
+}
+
+module.exports.readfileAndConvertToURL = readfileAndConvertToURL;
 
